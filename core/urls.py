a560240552b5cb251eef229f813af84480b3b26f
Carlos David <carlos.monier87@gmail.com>
2022-12-30 20:11:22 -0600

    Integrando CORE2

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import HomePageView, SamplePageView
+from . import views
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name="home"),
-    path('sample/', SamplePageView.as_view(), name="sample"),
+    path('', views.home, name="home"),
+    path('sample/', views.sample, name="sample"),
 ]
