d715bc35e86a5303fa0ff14f93921e7cacf1ea60
Carlos David <carlos.monier87@gmail.com>
2023-01-02 22:06:44 -0600

    1ER_CBV

--- core/views.py
+++ core/views.py
@@ -1,7 +1,12 @@
+from django.views.generic.base import TemplateView
 from django.shortcuts import render
 
-def home(request):
-    return render(request, "core/home.html")
+class HomePageView(TemplateView):
+    template_name = "core/home.html"
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'title':"Mi Super Web Playground"})
 
-def sample(request):
-    return render(request, "core/sample.html")
+
+class SamplePageView(TemplateView):
+    template_name = "core/sample.html"
