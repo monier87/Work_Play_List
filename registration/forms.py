aa28dfe20427d6688b1fae41448470ae84d963f8
Carlos David <carlos.monier87@gmail.com>
2023-01-08 19:48:16 -0600

    Crando Profile 2da Etapa

--- registration/forms.py
+++ registration/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class UserCreationFormWithEmail(UserCreationForm):
@@ -16,4 +17,15 @@ class UserCreationFormWithEmail(UserCreationForm):
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError("El email ya está registrado, prueba con otro.")
         return email
+    
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['avatar', 'bio', 'link']
+        widgets = {
+            'avatar': forms.ClearableFileInput(attrs={'class':'form-control-file mt-3'}),
+            'bio': forms.Textarea(attrs={'class':'form-control mt-3', 'rows':3, 'placeholder':'Biografía'}),
+            'link': forms.URLInput(attrs={'class':'form-control mt-3', 'placeholder':'Enlace'}),
+        }
 
--- registration/views.py
+++ registration/views.py
@@ -1,10 +1,11 @@
-from .forms import UserCreationFormWithEmail
+from .forms import UserCreationFormWithEmail, ProfileForm
 from django.views.generic import CreateView
-from django.views.generic.base import TemplateView
+from django.views.generic.edit import UpdateView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django import forms
+from .models import Profile
 
 # Create your views here.
 
@@ -30,5 +31,14 @@ class SignUpView(CreateView):
     
 
 @method_decorator(login_required, name='dispatch')
-class ProfileUpdate(TemplateView):
+class ProfileUpdate(UpdateView):
+    form_class= ProfileForm
+    success_url = reverse_lazy('profile')
     template_name = 'registration/profile_form.html'
+    
+    
+    
+    def get_object(self):
+        # recuperar el objeto que se va editar
+        profile, created= Profile.objects.get_or_create (user= self.request.user)
+        return profile
